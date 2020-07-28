81cf68d895405bffeb3816cca7133763d87ae736
abraham munguia <abrahamok.munguia@gmail.com>
2020-07-27 22:23:09 -0600

    cree modelo de contact y contexto

---
+++ Models/Contacts.cs
@@ -0,0 +1,10 @@
+namespace azureplatzi.Models
+{
+    public class Contacts
+    {
+        public string Identificador { get; set; }
+        public string Nombre { get; set; }
+        public string Email { get; set; }
+        public string Telefono { get; set; }
+    }
+}
