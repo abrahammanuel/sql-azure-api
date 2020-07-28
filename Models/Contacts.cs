9e6cbc5fc8571f023e54cfe6578db3cc2629f752
abraham munguia <abrahamok.munguia@gmail.com>
2020-07-28 17:00:46 -0600

    se agrego la directiva -Key- para hacer llave primaria a identificador

--- Models/Contacts.cs
+++ Models/Contacts.cs
@@ -1,8 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace azureplatzi.Models
 {
     public class Contacts
     {
-        public string Identificador { get; set; }
+        [Key]
+        public int Identificador { get; set; }
         public string Nombre { get; set; }
         public string Email { get; set; }
         public string Telefono { get; set; }
