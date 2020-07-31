de249de44c3043374358162ae749044e54c74289
abraham munguia <abrahamok.munguia@gmail.com>
2020-07-31 10:48:05 -0600

    added new methods of router for PUT;POST;DELETE in contact controller

--- Controllers/ContactController.cs
+++ Controllers/ContactController.cs
@@ -19,9 +19,53 @@ namespace azureplatzi.Controllers
         }
         // GET api/values
         [HttpGet]
-        public IEnumerable<Contacts> Get()
+        public ActionResult<IEnumerable<Contacts>> Get()
         {
             return contactsContext.ContactSet.ToList();
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<Contacts> Get(int id)
+        {
+            var selectedContact = (from c in contactsContext.ContactSet 
+                                    where c.Identificador == id
+                                    select c).FirstOrDefault();
+            return selectedContact;
+
+
+        }
+
+        // POST api/values
+        [HttpPost]
+        public IActionResult Post([FromBody] Contacts value)
+        {
+            Contacts newContact = value;
+            contactsContext.ContactSet.Add(newContact);
+            contactsContext.SaveChanges();
+            return Ok("tu contacto ha sido agregado");
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] Contacts value)
+        {
+            Contacts updatedContact = value;
+            var selectedElement = contactsContext.ContactSet.Find(id);
+            selectedElement.Nombre = value.Nombre;
+            selectedElement.Email = value.Email;
+            contactsContext.SaveChanges();
+            return Ok("contaco ha sido actualizado");
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            Contacts selectedElement = contactsContext.ContactSet.Find(id);
+            contactsContext.ContactSet.Remove(selectedElement);
+            contactsContext.SaveChanges();
+            return Ok($"contacto identificador:{id}  ha sido removido");
         }
         ~ContactController()
         {
